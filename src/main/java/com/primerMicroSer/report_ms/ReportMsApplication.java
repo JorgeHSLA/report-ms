2fe2e6820913cafc57071a257fbd7d91d0e9f97a
jorge <jorgesierralaiton@gmail.com>
2025-07-04 15:39:41 -0500

    et ste

---
+++ src/main/java/com/primerMicroSer/report_ms/ReportMsApplication.java
@@ -0,0 +1,14 @@
+package com.primerMicroSer.report_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//no se que oasqwe
+@SpringBootApplication
+public class ReportMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportMsApplication.class, args);
+	}
+
+}
